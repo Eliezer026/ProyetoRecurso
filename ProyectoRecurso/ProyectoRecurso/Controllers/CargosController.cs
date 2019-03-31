e3ead6496fb3571d32eec0d7c8f660b9728f29d3
Eliezer026 <macgiver018@gmail.com>
2019-03-30 23:55:43 -0400

    parte dos

---
+++ ProyectoRecurso/ProyectoRecurso/Controllers/CargosController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoRecurso.Models;
+
+namespace ProyectoRecurso.Controllers
+{
+    public class CargosController : Controller
+    {
+        private RecursoEntities db = new RecursoEntities();
+
+        // GET: Cargos
+        public ActionResult Index()
+        {
+            var cargo = db.Cargo.Include(c => c.Empleados);
+            return View(cargo.ToList());
+        }
+
+        // GET: Cargos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cargo cargo = db.Cargo.Find(id);
+            if (cargo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cargo);
+        }
+
+        // GET: Cargos/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdEmpleado = new SelectList(db.Empleados, "Id", "Nombre");
+            return View();
+        }
+
+        // POST: Cargos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,IdEmpleado,CargoEmpleado")] Cargo cargo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cargo.Add(cargo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdEmpleado = new SelectList(db.Empleados, "Id", "Nombre", cargo.IdEmpleado);
+            return View(cargo);
+        }
+
+        // GET: Cargos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cargo cargo = db.Cargo.Find(id);
+            if (cargo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdEmpleado = new SelectList(db.Empleados, "Id", "Nombre", cargo.IdEmpleado);
+            return View(cargo);
+        }
+
+        // POST: Cargos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,IdEmpleado,CargoEmpleado")] Cargo cargo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cargo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdEmpleado = new SelectList(db.Empleados, "Id", "Nombre", cargo.IdEmpleado);
+            return View(cargo);
+        }
+
+        // GET: Cargos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cargo cargo = db.Cargo.Find(id);
+            if (cargo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cargo);
+        }
+
+        // POST: Cargos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cargo cargo = db.Cargo.Find(id);
+            db.Cargo.Remove(cargo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
